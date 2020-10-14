725107052018d592e59451421d909249991c3381
Kevin Attinger <kevin.attinger@hs-furtwangen.de>
2020-10-14 15:13:43 +0200

    Add js and adjust some styles

--- js/task-one.js
+++ js/task-one.js
@@ -1,3 +1,28 @@
 $('.test-one').hover( () => {
-	$('')
+	$('.remove').css('display', 'none');
+	$('.active').removeClass('active');
+	$('.test-one').addClass('active');
+	$('.test-res-one').css('display', 'block');
 })
+
+$('.test-two').hover( () => {
+	$('.remove').css('display', 'none');
+	$('.active').removeClass('active');
+	$('.test-two').addClass('active');
+	$('.test-res-two').css('display', 'block');
+})
+
+$('.test-three').hover( () => {
+	$('.remove').css('display', 'none');
+	$('.active').removeClass('active');
+	$('.test-three').addClass('active');
+	$('.test-res-three').css('display', 'block');
+})
+
+$('.test-four').hover( () => {
+	$('.remove').css('display', 'none');
+	$('.active').removeClass('active');
+	$('.test-four').addClass('active');
+	$('.test-res-four').css('display', 'block');
+})
+
