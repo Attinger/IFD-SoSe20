237427e08da8986b54998bb6afe917c439676277
Kevin Attinger <kevin.attinger@hs-furtwangen.de>
2021-07-23 21:56:44 +0200

    finalizing my prototype

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,29 @@
 $('.menu--trigger').click( () => {
-    console.log('test');
+    $('.menu-icon').css('display', 'none');
+    $('.close-menu').css('display', 'block');
     $('.main-menu').css('transform', 'translateX(0)');
 })
+
+$('.close-menu').click( () => {
+    $('.close-menu').css('display', 'none');
+    $('.menu-icon').css('display', 'block');
+    $('.main-menu').css('transform', 'translateX(250px)');
+})
+
+$('.widget--trigger').click( () => {
+    $('.close-menu').css('display', 'none');
+    $('.menu-icon').css('display', 'block');
+    $('.main-menu').css('transform', 'translateX(250px)');
+    $('.add--widget').css('display', 'block');
+})
+
+$('.error-handler').click( () => {
+    $('.error-message').css('display', 'block');
+})
+
+$('.button--close').click( () => {
+    $('.error-message').css('display', 'none');
+    $('.add--widget').css('display', 'none');
+})
+
+
